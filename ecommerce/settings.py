86a4fadb08989ce7ab720cd92abafbff629a3315
ismat27 <adegbiteismail27@gmail.com>
2022-10-28 09:17:31 -0700

    rerun migrations

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
 
     # local apps
     'carts',
-    'products'
+    'products',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
