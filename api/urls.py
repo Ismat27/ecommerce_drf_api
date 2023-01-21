5a43cf12104ca48c885e6b57b30c17787b002d66
ismat27 <adegbiteismail27@gmail.com>
2023-01-21 08:05:22 -0800

    create route for logging in

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from users.views import UserCreationApi, UserDetailApi
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('products/', include('products.urls')),
     path('carts/', include('carts.urls')),
     path('signup/', UserCreationApi.as_view()),
-    path('users/<int:pk>/', UserDetailApi.as_view())
+    path('users/<int:pk>/', UserDetailApi.as_view()),
+    path('login/', obtain_auth_token)
 ]
