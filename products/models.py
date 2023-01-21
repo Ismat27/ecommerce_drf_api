6afb62892cbc8fbd72008cbca0ab0a6300d6bfec
ismat27 <adegbiteismail27@gmail.com>
2023-01-21 08:10:23 -0800

    Created category model and linked it to product model

--- products/models.py
+++ products/models.py
@@ -5,8 +5,18 @@ def upload_to(instance, filename):
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     desc = models.TextField(blank=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, null=False, blank=False)
     image = models.ImageField(upload_to=upload_to, null=True, blank=True)
+    category =models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
