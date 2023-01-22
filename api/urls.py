e44ec4d5aa1bdea27ae3566237ee0760bba7917c
ismat27 <adegbiteismail27@gmail.com>
2023-01-22 05:56:11 -0800

    updated view for user login

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from users.views import UserCreationApi, UserDetailApi
-from rest_framework.authtoken.views import obtain_auth_token
+from users.views import UserLogin
 
 urlpatterns = [
     path('products/', include('products.urls')),
     path('carts/', include('carts.urls')),
     path('signup/', UserCreationApi.as_view()),
     path('users/<int:pk>/', UserDetailApi.as_view()),
-    path('login/', obtain_auth_token)
+    path('login/', UserLogin.as_view())
 ]
