bdd0f088212484c8ca9fd601ae940fa7723c7fd1
ismat27 <adegbiteismail27@gmail.com>
2023-01-23 13:31:42 -0800

    new url for getting product of a particular category

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductListCreate, ProductRetrieveUpdateDelete
+from .views import ProductListCreate, ProductRetrieveUpdateDelete, CategoryProduct
 
 urlpatterns = [
     path('', ProductListCreate.as_view()),
-    path('<pk>/', ProductRetrieveUpdateDelete.as_view())
+    path('<int:pk>/', ProductRetrieveUpdateDelete.as_view()),
+    path('<str:category_name>/', CategoryProduct.as_view())
 ]
