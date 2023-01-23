c3f8879612322281a86588e55ae97287f6447024
ismat27 <adegbiteismail27@gmail.com>
2023-01-23 10:47:26 -0800

    added new four columns to the product table

--- products/models.py
+++ products/models.py
@@ -15,8 +15,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     desc = models.TextField(blank=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, null=False, blank=False)
+    discount_price = models.DecimalField(max_digits=15, decimal_places=2, null=False, blank=False)
+    in_stock = models.BooleanField(default=True, null=False, blank=False)
+    is_flash_sale = models.BooleanField(default=False, null=False, blank=False)
+    quantity = models.IntegerField(default=1, null=False, blank=False)
     image = models.ImageField(upload_to=upload_to, null=True, blank=True)
-    category =models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
