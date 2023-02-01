f864c33f7dbef3edfda1d53d7334af503a670b0e
ismat27 <adegbiteismail27@gmail.com>
2023-02-01 05:28:45 -0800

    formatted and added user review model to admin dashboard

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Product, Category, Brand
+from .models import Product, Category, Brand, UserReview
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'desc', 'price')
 
+class UserReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'review', 'rating')
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(UserReview, UserReviewAdmin)
